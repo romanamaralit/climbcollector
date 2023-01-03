8e5cbb96dbe239e3e0ec8edc4e5d413b921ecd1b
romanamaralit <romana.maralit@gmail.com>
2023-01-02 23:33:30 -0800

    Register the Climb model with the admin portal

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
+from .models import Climb
 
-
-# Add the Climb class & list and view function below the imports
-class Climb:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, rating, description, location, people):
-    self.name = name
-    self.rating = rating
-    self.description = description
-    self.location = location
-    self.people = people
-
-climbs = [
-  Climb('Change', '5.15c (9b+)', 'Only one person has ever completed this climb.', 'Flatanger, Norway', 1),
-  Climb('La Dura Dura', '9b+ (5.15c)', 'Two people have ascended this climb.', 'Peramola, Spain', 2 ),
-  Climb('La Rage d’Adam', '9a (5.15d)', 'La rage de dent means toothache', 'Verdon Gorge , France', 0)
-]
 
 # Define the home view
 #create your views here
@@ -25,4 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def climbs_index(request):
+  climbs=Climb.objects.all()
   return render(request, 'climbs/index.html', { 'climbs': climbs })

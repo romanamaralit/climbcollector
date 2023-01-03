c189e1ae33af1b25ec25198998e233616b477e73
romanamaralit <romana.maralit@gmail.com>
2023-01-02 20:27:57 -0800

    Create climb class

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # route for climbs index
+    path('climbs/', views.climbs_index, name='index'),
 ]
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,21 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+# Add the Climb class & list and view function below the imports
+class Climb:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, rating, description, location, people):
+    self.name = name
+    self.rating = rating
+    self.description = description
+    self.location = location
+    self.people = people
+
+climbs = [
+  Climb('Change', '5.15c (9b+)', 'Only one person has ever completed this climb.', 'Flatanger, Norway', 1),
+  Climb('La Dura Dura', '9b+ (5.15c)', 'Two people have ascended this climb.', 'Peramola, Spain', 2 ),
+  Climb('La Rage d’Adam', '9a (5.15d)', 'La rage de dent means toothache', 'Verdon Gorge , France', 0)
+]
+
 # Define the home view
 #create your views here
 def home(request):
@@ -10,3 +25,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def climbs_index(request):
+  return render(request, 'climbs/index.html', { 'climbs': climbs })

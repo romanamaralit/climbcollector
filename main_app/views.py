ef43b3e3a5be2a78ab8c9cdfddb9485cb6f2359a
romanamaralit <romana.maralit@gmail.com>
2023-01-02 19:15:55 -0800

    Make about page route and base template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+#create your views here
+def home(request):
+  return HttpResponse('<h1>Home Page</h1>')
+
+def about(request):
+  return render(request, 'about.html')
